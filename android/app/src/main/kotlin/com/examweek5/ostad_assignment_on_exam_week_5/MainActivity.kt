2d45685da78f189c266e4000d72b5944ff31b047
Faysal Ahmed <techbanglamaxfs@gmail.com>
2025-11-17 21:14:16 +0600

    first commit

---
+++ android/app/src/main/kotlin/com/examweek5/ostad_assignment_on_exam_week_5/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.examweek5.ostad_assignment_on_exam_week_5
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/app.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:ostad_assignment_on_exam_week_5/home_screen.dart';
+
+class AssignmentExamWeek5 extends StatefulWidget {
+  const AssignmentExamWeek5({super.key});
+
+  @override
+  State<AssignmentExamWeek5> createState() => _AssignmentExamWeek5State();
+}
+
+class _AssignmentExamWeek5State extends State<AssignmentExamWeek5> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
